7b11a8a10575d07fbc531703b759b189cdd085f3
Alua Baikadamova <abaikad00@gmail.com>
2018-12-16 08:54:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(array)# code an argument h
 end
 
 def long_planeteer_calls(array)# code an argument here
- array.all? { |x| x.length>4 } 
+ array.any? { |x| x.length>4 } 
 end
 
 def find_the_cheese(array)# code an argument here
