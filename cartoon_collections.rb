e5a2fd09e5c76c50588c256fdab470ac3af84ca2
Alua Baikadamova <abaikad00@gmail.com>
2018-12-16 08:48:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ return a
 end
 
 def summon_captain_planet(array)# code an argument h
-  array.collect{ |x| x.charAt(0).toUpperCase() +x.slice(1)+ "!" }  
+  array.collect{ |x| x.first.upcase+ "!" }  
 
 end
 
