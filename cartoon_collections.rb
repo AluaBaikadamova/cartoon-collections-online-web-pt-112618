d13f6e5bf79c706ec6dcfb739918f0e44a8f4317
Alua Baikadamova <abaikad00@gmail.com>
2018-12-16 08:27:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def find_the_cheese(array)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
 if (array.length>0)
   array.each do |x| 
-  if cheese_types.include?(x) =true
+  if cheese_types.include?(x) 
     return x
     exit
   end
